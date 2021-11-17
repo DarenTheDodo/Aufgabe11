97340d8303497db2133870586673b6e1cbe10186
Falere@outlook.com <Falere@outlook.com>
2021-11-17 11:07:37 +0100

    intitial commit

---
+++ cmake-build-debug/CMakeFiles/Aufgabe11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Aufgabe11.exe"
+  "Aufgabe11.exe.manifest"
+  "Aufgabe11.pdb"
+  "CMakeFiles/Aufgabe11.dir/main.c.obj"
+  "libAufgabe11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Aufgabe11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// cgrahl3s, kzande3s
+// Dieses Programm soll für eine Zahl x und einer Genauigkeit n die zugehörige Quadratwurzel von x ausgegeben
+// Test:
+// x = 2, n = 4, w = 1.4142; x = 49, n = 10, w = 7; x = 81, n = 10, w = 9;
+// Was passiert bei wachsendem n?
+// Je höher die Genauigkeit n, desto präziser ist das Ergebnis (bei hohen x Werten sind die Ergebnisse erst nach einigen Durchläufen annähernd richtig)
+int main() {
+    double w, x;
+    int n;
+    w = 1;
+
+    printf("x=");
+    scanf("%lf", &x);
+    printf("n=");
+    scanf("%i", &n);
+
+    for (int i = 0; i <= n; i++) {
+        w = 0.5 * (w + x / w);
+        printf("%lf\n", w);
+    }
+    //printf("%lf\n", w);
+    return 0;
+}
+
